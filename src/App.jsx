7847aba2f9d3c9e325bb410212a2a00d0c48793f
Andrii Chychkan <mywork1728@gmail.com>
2024-04-11 16:59:02 +0300

    added base usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import tasks from "./tasks";
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(tasks);
+  const [taskList, setTaskList] = useState(tasks);
+  const taskAddFunction = (e) => {
+    event.preventDefault();
+    console.log(e.target);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form
+        action=""
+        onSubmit={(e) => {
+          taskAddFunction(e);
+        }}
+      >
+        <input type="text" />
+        <button type="submit">Add</button>
+      </form>
+
+      <ul>
+        {taskList.map((el) => {
+          return (
+            <li key={el.id}>
+              {el.id} {el.content} <button>Delete</button> <button>Done</button>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/tasks.js
@@ -0,0 +1,16 @@
+const tasks = [
+  {
+    id: 1,
+    content: "walk dog",
+  },
+  {
+    id: 2,
+    content: "go to work",
+  },
+  {
+    id: 3,
+    content: "listen to music ('Black sabbath')",
+  },
+];
+
+export default tasks;
