8d2e28acf0ad77072ffedf049d79d9488cb125d5
Andrii Chychkan <mywork1728@gmail.com>
2024-04-16 17:34:12 +0300

    wtf

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,33 @@ import { create } from "zustand";
 function App() {
   const [inputText, setInputText] = useState("");
 
-  const useTasks = create((set) => ({
-    tasks: [],
+  const useStore = create((set) => ({
+    tasks: [
+      { id: "fdfsdfsdf", task: "lolo lolo lo" },
+      { id: "dwefe", task: "roooo koko ko" },
+    ],
+    count: 0,
     addTask: (text) =>
-      set((state) => {
-        const newTask = { id: uid(), task: text };
-        return { tasks: [...state.tasks, newTask] };
-      }),
+      set((state) => ({
+        tasks: [{ id: uid(), task: text }, ...state.tasks],
+      })),
+    plus: () => set((state) => ({ count: state.count + 1 })),
   }));
 
-  const addTask = useTasks((state) => state.addTask);
-  const tasks = useTasks((state) => state.tasks);
+  const addTask = useStore((state) => state.addTask);
+  const tasks = useStore((state) => state.tasks);
+
+  const { count, plus } = useStore();
 
   const addTaskToList = (e) => {
     e.preventDefault();
-    addTask(inputText);
+    plus;
+    addTask({ text: inputText });
     setInputText("");
   };
 
   const inputChange = (e) => {
     setInputText(e.target.value);
-  };
-
-  const deleteTaskFromList = (id) => {
-    dispatch(deleteTask(id));
   };
 
   return (
@@ -48,19 +51,13 @@ function App() {
         />
         <button type="submit">Add</button>
       </form>
+      <div>{count}</div>
       <ul>
         {tasks.map((el) => {
           return (
             <li key={el.id}>
               {el.task}
-              <button
-                className="del"
-                onClick={() => {
-                  deleteTaskFromList(el.id);
-                }}
-              >
-                Delete
-              </button>
+              <button className="del">Delete</button>
               <button>Done</button>
             </li>
           );
@@ -71,3 +68,23 @@ function App() {
 }
 
 export default App;
+
+// const useStore = create((set) => ({
+//   count: 0,
+//   plus: () => set((state) => ({ count: state.count + 1 })),
+// }));
+
+// export default function App() {
+//   const { plus, count } = useStore();
+
+//   return (
+//     <main>
+//       <h2>{count}</h2>
+//       <div className="btn-box">
+//         <button onClick={plus} className="btn decrement">
+//           +
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
