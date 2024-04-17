805a4bc29951b317548fcbc8ccb4fd6592ffd482
Andrii Chychkan <mywork1728@gmail.com>
2024-04-17 16:39:03 +0300

    zustand done

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,20 @@ import { uid } from "uid";
 import { create } from "zustand";
 
 const useStore = create((set) => ({
-  tasks: [
-    { id: "fdsfs", task: "do something" },
-    { id: "kokn", task: "do something else" },
-  ],
+  tasks: [],
   addTask: (text) =>
     set((state) => ({
       tasks: [...state.tasks, { id: uid(), task: text }],
     })),
   deleteTask: (id) =>
     set((state) => ({
-      tasks: [...state.tasks, { id: uid(), task: text }],
+      tasks: state.tasks.filter((task) => task.id !== id),
     })),
 }));
 
 export default function App() {
   const [inputText, setInputText] = useState("");
-  const { addTask, tasks } = useStore();
+  const { addTask, tasks, deleteTask } = useStore();
 
   const inputChange = (e) => {
     setInputText(e.target.value);
@@ -56,8 +53,14 @@ export default function App() {
           return (
             <li key={el.id}>
               {el.task}
-              <button className="del">Delete</button>
-              <button>Done</button>
+              <button
+                className="del"
+                onClick={() => {
+                  deleteTask(el.id);
+                }}
+              >
+                Delete
+              </button>
             </li>
           );
         })}
